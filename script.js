f277e9006c82365b7d413a8aba7e6cf8bbcee68e
Anna shabbagh <afreenshabbagh@gmail.com>
2024-10-29 14:10:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+document.addEventListener("DOMContentLoaded", () =>{
+	const images = document.querySelectorAll("div[class^='img']");
+	const resetButton = document.getElementById("reset");
+	images.
+})
