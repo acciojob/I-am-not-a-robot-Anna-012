270f2914cfa542eb0e1f37ac5130c6d8a59a3491
Anna shabbagh <afreenshabbagh@gmail.com>
2024-10-30 22:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,76 @@
 //your code here
-document.addEventListener("DOMContentLoaded", () =>{
-	const images = document.querySelectorAll("div[class^='img']");
-	const resetButton = document.getElementById("reset");
-	images.forEach(image =>{
-		image.addEventListener("click", ()=>{
-			resetButton.style.display='block';
-		})
-	})
-	  resetButton.addEventListener("click", () => {
-        resetButton.style.display = "none";
-	})
-})
+  // const images = [
+  //   'https://picsum.photos/id/237/200/300',
+  //   'https://picsum.photos/seed/picsum/200/300',
+  //   'https://picsum.photos/200/300?grayscale',
+  //   'https://picsum.photos/200/300/',
+  //   'https://picsum.photos/200/300.jpg'
+  //  ];
+
+  //  function generateImages(){
+  //   var selectedImageIndex = Math.floor(Math.random()*images.length); //1 , 4,2,3, 0,5
+  //   var selectedImage = images[selectedImageIndex];
+  //   var allImages = images.concat(selectedImage);
+  //   var shuffledImages = allImages.sort(function(){
+  //       return 0.5 - Math.random();
+  //   });
+
+    
+  //   var imageContainer = document.getElementById('image-container');
+  //   imageContainer.innerHTML = '';
+  //   for(var index = 0; index < shuffledImages.length; index++){
+  //       var src = shuffledImages[index];
+  //       var img = document.createElement('img');
+  //       img.src = src;
+  //       img.className = 'img' +(index+1);
+  //       img.dataset.index = index;
+  //       img.addEventListener('click', handleImageClick);
+  //       imageContainer.appendChild(img);
+  //   }; 
+  //  }
+
+  //  let firstClick = null;
+  //  let secondClick = null;
+
+  //  function handleImageClick(event){
+  //   const clickedImage = event.target;
+  //   if(firstClick == clickedImage || secondClick == clickedImage ) return;
+
+  //   document.getElementById('reset').style.display = 'block';
+
+  //   if(!firstClick) {
+  //       firstClick = clickedImage;
+  //   }else if(!secondClick){
+  //       secondClick = clickedImage;
+  //       document.getElementById('verify').style.display = 'block';
+  //   }
+
+    
+  //  }
+
+  //  function reset(){
+  //   firstClick = null;
+  //   secondClick = null;
+  //   document.getElementById('reset').style.display = 'none';
+  //   document.getElementById('verify').style.display = 'none';
+  //   document.getElementById('para').style.display = 'none';
+  //   document.getElementById('image-container').innerHTML = '';
+  //   generateImages();
+  //  }
+
+  //  function verify(){
+  //   if(firstClick && secondClick){
+  //       if(firstClick.src === secondClick.src){
+  //           document.getElementById('para').innerHTML = 'Verified';
+
+  //       }else{
+  //           document.getElementById('para').innerHTML = 'Not a human';
+  //       }
+
+  //       document.getElementById('para').style.display = 'block';
+  //   }
+  //  }
+  //  document.getElementById('reset').addEventListener('click', reset);
+  //  document.getElementById('verify').addEventListener('click', verify);
+
+  //  generateImages();
